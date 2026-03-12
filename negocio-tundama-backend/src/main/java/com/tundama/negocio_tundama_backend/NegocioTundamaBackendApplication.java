2f8dc3a09a4fd499cece0a74a329f390d0a71a0f
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-03-11 19:07:39 -0500

    Se normaliza app

---
+++ negocio-tundama-backend/src/main/java/com/tundama/negocio_tundama_backend/NegocioTundamaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tundama.negocio_tundama_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegocioTundamaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NegocioTundamaBackendApplication.class, args);
+	}
+
+}
