f5c48d69aa7456be9f270c2987af968053dcb3be
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-03-11 19:08:26 -0500

    Se normaliza test

---
+++ negocio-tundama-backend/src/test/java/com/tundama/negocio_tundama_backend/NegocioTundamaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tundama.negocio_tundama_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NegocioTundamaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
