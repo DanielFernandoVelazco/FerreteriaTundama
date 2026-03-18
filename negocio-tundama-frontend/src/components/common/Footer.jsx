0e655f5f759dc145ed59ef9c76dee3c07964f772
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-03-17 21:19:08 -0500

    Se normaliza common footer jsx

---
+++ negocio-tundama-frontend/src/components/common/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>NEGOCIO TUNDAMA LTDA</p>
+      <p>TODOS LOS DERECHOS RESERVADOS</p>
+    </footer>
+  );
+};
+
+export default Footer;
