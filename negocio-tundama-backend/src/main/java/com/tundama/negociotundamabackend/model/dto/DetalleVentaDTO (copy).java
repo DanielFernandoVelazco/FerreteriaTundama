53b204d1f7b7b1854e1d675015e93d55d4d89c21
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-04-17 21:52:07 -0500

    se normaliza venta

---
+++ negocio-tundama-backend/src/main/java/com/tundama/negociotundamabackend/model/dto/DetalleVentaDTO (copy).java
@@ -0,0 +1,13 @@
+package com.tundama.negociotundamabackend.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DetalleVentaDTO {
+    private Long productoId;
+    private Integer cantidad;
+}
